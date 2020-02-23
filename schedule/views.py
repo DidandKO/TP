034aa5012358514fc9eaeb9c94d1c1727136f5e3
Did&K_O <didenko.nazar@list.ru>
2020-02-23 13:47:57 +0700

    DONE

--- schedule/views.py
+++ schedule/views.py
@@ -7,6 +7,7 @@ def upload(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['document']
         print(uploaded_file.name)
+        uploaded_file.name = 'DATATP.xlsx'
         print(uploaded_file.size)
         fs = FileSystemStorage()
         fs.save(uploaded_file.name, uploaded_file)
