e97c64a02396aa22d54a81e445ffd3cc36ee6e7f
Did&K_O <didenko.nazar@list.ru>
2020-03-23 10:16:19 +0700

    I dont know what i should do

--- schedule/models.py
+++ schedule/models.py
@@ -7,3 +7,7 @@ class Amperage(models.Model):
     operation = models.IntegerField()
     axis = models.IntegerField()
     value = models.DecimalField(max_digits=20, decimal_places=14)
+
+
+class Greeting(models.Model):
+    when = models.DateTimeField("date created", auto_now_add=True)
